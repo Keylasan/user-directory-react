4dafaada783ef06de87296431fa6767389051e54
Life is Now <keylasan28@yahoo.com>
2020-06-06 21:27:01 -0400

    started sorting employees

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { find } from "../../index"
 import "./style.css";
+import { femaleEmployee } from "../../index"
+import { maleEmployee } from "../../index"
+
+
 function Search() {
   find()
   return (
@@ -8,6 +12,10 @@ function Search() {
       <div className="row">
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
           <button onClick={find}>Search</button>
+          <input type="text" id="filterNationality"></input>
+        <button onClick={femaleEmployee}>Female Employees</button>
+        <button onClick={maleEmployee}>Male Employees</button>
+        <button onClick={nonBinaryEmployee}>Non Binary Employees</button>
         </div>
         <div id="content"></div>
       </div>
--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,13 @@ import axios from 'axios'
 import registerServiceWorker from "./registerServiceWorker";
 
 var results;
+
+//sorting by gender
+femaleEmployee =[];
+maleEmployee =[];
+
 export function find() {
-    var queryURL = "https://randomuser.me/api/?results=200&nat=us";
+    var queryURL = "https://randomuser.me/api/?results=33";
     return axios
         .get(queryURL).then(response => {
             results = response.data.results
@@ -21,17 +26,41 @@ export function newEmployeeRow() {
         const div = document.createElement('div');
         div.innerHTML = `
         <div id="employee-`+i+`
-            <p id="firstName-`+i+`">Name: `+results[i].name.first+`</p>
-            <p id="lastName-`+i+`">Name: `+results[i].name.last+`</p>
-           
+            <p id="firstName-`+i+`">First Name: `+results[i].name.first+`</p>
+            <p id="lastName-`+i+`">Last Name: `+results[i].name.last+`</p>
+            <p id="gender-`+i+`">Gender: `+results[i].gender+`</p>
             <p id="email-`+i+`">Email: `+results[i].email+`</p>
-            <p id="number-`+i+`">Cell: `+results[i].cell+`</p>
+            <p id="number-`+i+`">Phone: `+results[i].cell+`</p>
             <img id="image-`+i+`" src="`+results[i].picture.thumbnail+`">
         </div>
         `;
         document.getElementById('content').appendChild(div);
     }
 }
+//sorting by gender
+femaleEmployee =[];
+maleEmployee =[];
+nonBinaryEmployee= "I'm Sorry; There are no employees found in our system under that category. Try again later."
+
+
+export function FemaleEmployees(){
+    femaleEmployee =[];
+    for(var i = 0; i < results.length; i++){
+        if (results[i].gender === "female"){
+femaleEmployee.push(results[i]);
+        }
+    }
+}
+
+export function MaleEmployees(){
+    maleEmployee =[];
+    for(var i = 0; i < results.length; i++){
+        if (results[i].gender === "male"){
+maleEmployee.push(results[i]);
+        }
+    }
+}
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
